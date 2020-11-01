e6bfef6302c4de3632bcf9e61dbb2e06051faf0a
usergitname <68472275+usergitname@users.noreply.github.com>
2020-10-31 23:53:45 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ChatBot',
+    version='',
+    packages=['chatbot', 'chatbotapp', 'chatbotapp.migrations'],
+    url='',
+    license='',
+    author='Joshi.Cleetus',
+    author_email='',
+    description=''
+)
